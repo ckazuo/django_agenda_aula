89e7f04483a2b7f672da7e76e4ec042fed0d81b9
Cesar Kazuo Miya <ckazuo@gmail.com>
2022-05-21 18:07:36 -0300

    login e novo campo em tabela Agenda

--- aulas/admin.py
+++ aulas/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
 
-from .models import Aulas, Schedule, Aluno
+from .models import Aulas, Agenda, Aluno
 
 # Register your models here.
 admin.site.register(Aulas)
-admin.site.register(Schedule)
+admin.site.register(Agenda)
 admin.site.register(Aluno)
