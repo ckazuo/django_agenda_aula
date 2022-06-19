a2c21664d3a8a63a360f18f70e143f50e4162bb7
Cesar Kazuo Miya <ckazuo@gmail.com>
2022-06-18 21:30:31 -0300

    add first test

--- aulas/tests.py
+++ aulas/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
+import unittest
+from django.test import Client
 
 # Create your tests here.
+class SimpleTest(unittest.TestCase):
+    def setUp(self) -> None:
+        self.client = Clietn()
+        return super().setUp()
+
+    def test_indexpageresponse(self):
+
+        # Issue a GET request.
+        response = self.client.get('index')
+
+        # Check that the response is 200 OK.
+        self.assertEqual(response.status_code, 200)
